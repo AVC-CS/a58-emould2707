a04b9689a7bc13d1f875d261ac09d97ad751430b
emould2707 <emould@students.avc.edu>
2026-03-31 20:45:58 -0700

    a58 done

--- main_read.cpp
+++ main_read.cpp
@@ -11,16 +11,25 @@ int main()
     ifstream ifs;
 
     // TODO: open "students.txt" for reading
-
-    // TODO: check if file opened successfully; print error and exit if not
-
-    // TODO: read M from ifs, print "Total M students"
+    ifs.open("students.txt");
+    if (!ifs){
+        cout << "Error opening file" << endl;
+        exit(0);
+    }
+    ifs >> M;
+    cout << "Total " << M << " students" << endl;
 
     for (int i = 0; i < M; i++)
     {
-        // TODO: read stuName, score1, score2 from ifs
-        // TODO: compute sum = score1 + score2, avg = sum / 2.0
-        // TODO: print Student Name, score1, score2, Sum, Avg
+        ifs >> stuName >> score1 >> score2;
+        sum = score1 + score2;
+        avg = sum/2.0;
+        cout << "Student Name: " << stuName 
+        << " score1: "<< score1 << " score2: " 
+        << score2 << " Sum: " << sum 
+        << " Avg: "<< avg 
+        << endl;
+        
     }
     ifs.close();
     return 0;
